fdd4c8ef5ecb5a3dd7dfa0b8a5bfb0ec84ca1e0d
isaacpmoll <73903177+isaacpmoll@users.noreply.github.com>
2021-01-19 13:03:10 -0500

    Update Rock.py

--- Rock.py
+++ Rock.py
@@ -1,10 +1,40 @@
 
-import
+import random
 
 personPlay = ""
 computerPlay = ""
+clist = ["r", "p", "s"]
 
-computerInt = random()
+personPlay = input("Enter R, P, or S: ")
+computerPlay = random.choice(clist)
+personPlay = personPlay.lower()
+
+print("Computer chose: " + computerPlay)
+
+if personPlay=="r":
+	if computerPlay=="r":
+		print("tie")
+	elif computerPlay=="p":
+		print("lose")
+	else:
+		print("win!")
+elif personPlay=="p":
+	if computerPlay=="r":
+		print("win!")
+	elif computerPlay=="p":
+		print("tie")
+	else:
+		print("lose")
+elif personPlay=="s":
+	if computerPlay=="r":
+		print("lose")
+	elif computerPlay=="p":
+		print("win!")
+	else:
+		print("tie")
+else:
+	print("what")
+
 
 # get player's play 
 
